811f487658d262b82ef3d0aa8ffb1e7b7966bbdf
Dmitriy <up@websector.ru>
2021-03-31 18:54:29 +0300

    add render feed/post and some ref

--- src/view.js
+++ src/view.js
@@ -7,8 +7,17 @@ const buildFeedbackElement = (textFeedback, feedbackClass) => {
   return divEl;
 };
 const initView = (state, elements) => {
+  const {
+    input,
+    button,
+    form,
+    formBox,
+    feedsBox,
+    postsBox,
+  } = elements;
+
   const renderFeedback = (textFeedback, feedbackClass) => {
-    const oldFeedbackEl = document.querySelector(`.${feedbackClass}`);
+    const oldFeedbackEl = document.querySelector('.feedback');
     if (oldFeedbackEl) {
       oldFeedbackEl.remove();
     }
@@ -16,37 +25,99 @@ const initView = (state, elements) => {
       return;
     }
     const errorEl = buildFeedbackElement(textFeedback, feedbackClass);
-    elements.formBox.append(errorEl);
+    formBox.append(errorEl);
   };
 
   const renderInput = (status) => {
     switch (status) {
       case 'filling':
-        elements.input.classList.remove('is-invalid');
+        input.classList.remove('is-invalid');
         break;
       case 'invalid':
-        elements.input.classList.add('is-invalid');
-        elements.input.removeAttribute('disabled');
-        elements.button.removeAttribute('disabled');
+        input.classList.add('is-invalid');
+        input.removeAttribute('disabled');
+        button.removeAttribute('disabled');
         break;
       case 'downloading':
-        elements.input.setAttribute('disabled', true);
-        elements.button.setAttribute('disabled', true);
+        input.setAttribute('disabled', true);
+        button.setAttribute('disabled', true);
         break;
       case 'success':
-        renderFeedback('RSS успешно загружен!', 'text-success');
-        elements.input.removeAttribute('disabled');
-        elements.button.removeAttribute('disabled');
-        elements.form.reset();
+        input.removeAttribute('disabled');
+        button.removeAttribute('disabled');
+        form.reset();
+        break;
+      case 'failed':
+      case 'parseFailed':
+        input.removeAttribute('disabled');
+        button.removeAttribute('disabled');
         break;
       default:
         throw Error(`Unknown form status ${status}`);
     }
   };
 
+  const renderFeedEl = (title, description) => {
+    const feedEl = document.createElement('li');
+    feedEl.classList.add('list-group-item');
+    const titleEl = document.createElement('h3');
+    titleEl.textContent = title;
+    const descriptionEl = document.createElement('p');
+    descriptionEl.textContent = description;
+    feedEl.append(titleEl);
+    feedEl.append(descriptionEl);
+    return feedEl;
+  };
+
+  const renderFeeds = (feeds) => {
+    feedsBox.innerHTML = '';
+    const caption = document.createElement('h2');
+    caption.textContent = 'Фиды';
+    feedsBox.prepend(caption);
+    const feedsList = document.createElement('ul');
+    feedsList.classList.add('list-group', 'mb-5');
+    feedsBox.append(feedsList);
+    feeds.forEach((feed) => {
+      const feedEl = renderFeedEl(feed.title, feed.description);
+      feedsList.prepend(feedEl);
+    });
+  };
+
+  const renderPostEl = (title, description, link) => {
+    const postEl = document.createElement('li');
+    postEl.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start');
+    const linkEl = document.createElement('a');
+    linkEl.setAttribute('href', link);
+    linkEl.textContent = title;
+    const postButton = document.createElement('button');
+    postButton.setAttribute('type', 'button');
+    postButton.classList.add('btn', 'btn-primary', 'btn-sm');
+    postButton.textContent = 'Просмотр';
+    postEl.append(linkEl);
+    postEl.append(postButton);
+    return postEl;
+  };
+
+  const renderPosts = (posts) => {
+    postsBox.innerHTML = '';
+    const caption = document.createElement('h2');
+    caption.textContent = 'Посты';
+    postsBox.prepend(caption);
+    const postsList = document.createElement('ul');
+    postsList.classList.add('list-group');
+    postsBox.append(postsList);
+    posts.forEach((post) => {
+      const postEl = renderPostEl(post.title, post.description, post.link);
+      postsList.prepend(postEl);
+    });
+  };
+
   const mapping = {
     'form.status': () => renderInput(state.form.status),
     'form.error': () => renderFeedback(state.form.error, 'text-danger'),
+    'form.feedback': () => renderFeedback(state.form.feedback, 'text-success'),
+    feeds: () => renderFeeds(state.feeds),
+    posts: () => renderPosts(state.posts),
   };
 
   const watched = onChange(state, (path) => {

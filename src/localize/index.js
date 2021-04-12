ed54aa4e4c63c0a291986f38a03add29044ba34e
Dmitriy Gerasimov <up@websector.ru>
2021-04-13 00:28:49 +0300

    temporary disable autodetect locales (for hexlet-check)

--- src/localize/index.js
+++ src/localize/index.js
@@ -1,10 +1,11 @@
 import i18next from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+// import LanguageDetector from 'i18next-browser-languagedetector';
 import en from '../locales/en.js';
 import ru from '../locales/ru.js';
 
 const options = {
   fallbackLng: 'en',
+  lng: 'ru',
   resources: {
     en: {
       translation: en,
@@ -16,7 +17,8 @@ const options = {
 };
 
 const localize = () => {
-  i18next.use(LanguageDetector).init(options);
+  // i18next.use(LanguageDetector).init(options);
+  i18next.init(options);
   return i18next;
 };
 

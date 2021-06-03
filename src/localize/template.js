f502f9748976634d3519db1d3f2bf021416d0e84
Dmitriy Gerasimov <up@websector.ru>
2021-06-03 17:34:57 +0300

    some changes in the template

--- src/localize/template.js
+++ src/localize/template.js
@@ -1,13 +1,13 @@
 const translateHomePage = (i18n) => {
   const elements = {
     title: [document.querySelector('title'), 'textContent'],
-    headline: [document.querySelector('h1.display-4'), 'textContent'],
-    tagline: [document.querySelector('p.lead'), 'textContent'],
-    sample: [document.querySelector('p.text-muted'), 'textContent'],
-    placeholder: [document.querySelector('input.form-control-lg'), 'placeholder'],
-    add: [document.querySelector('button.btn.btn-lg.btn-primary'), 'textContent'],
-    read: [document.querySelector('a.full-article'), 'textContent'],
-    close: [document.querySelector('button.btn.btn-secondary'), 'textContent'],
+    headline: [document.querySelector('h1'), 'textContent'],
+    tagline: [document.querySelector('p#tagline'), 'textContent'],
+    sample: [document.querySelector('p#sample'), 'textContent'],
+    placeholder: [document.querySelector('label[for="url"]'), 'textContent'],
+    add: [document.querySelector('button#add'), 'textContent'],
+    read: [document.querySelector('a#full-article'), 'textContent'],
+    close: [document.querySelector('button#close-modal'), 'textContent'],
   };
   Object.entries(elements).forEach((element) => {
     const [key, [domEl, attribute]] = element;
